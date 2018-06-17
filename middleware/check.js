273bd561330ba46519e16ffdcf45ff1c16b74789
linhaotxl <6025235872qq.com>
2018-06-17 23:50:09 +0800

    添加中间件文件

---
+++ middleware/check.js
@@ -0,0 +1,25 @@
+
+/**
+ * 检查是否登录的中间件
+ * @param { Object } req 请求信息
+ * @param { Object } res 响应信息
+ * @param { Function } next 执行下一个中间件的方法
+ */
+const checkLoginin = function ( req, res, next ) {
+
+}
+
+/**
+ * 检查是否登录的中间件
+ * @param { Object } req 请求信息
+ * @param { Object } res 响应信息
+ * @param { Function } next 执行下一个中间件的方法
+ */
+const checkNotLoginin = function ( req, res, next ) {
+
+}
+
+module.exports = {
+    checkLoginin,
+    checkNotLoginin
+};
